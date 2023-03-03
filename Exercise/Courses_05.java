81241261ef34da2098b7a605986796a7f3dcbd59
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-03-04 00:20:54 +0200

    Commit

---
+++ Exercise/Courses_05.java
@@ -0,0 +1,33 @@
+package MapsLambdaAndStreamAPI.Exercise;
+
+import java.util.*;
+
+public class Courses_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command  = scanner.nextLine();
+        LinkedHashMap<String, List<String>> coursesList = new LinkedHashMap<>();
+
+        while (!command.equals("end")) {
+            String[] commandArr = command.split(" : ");
+
+            String course = commandArr[0];
+            String student = commandArr[1];
+
+            if (!coursesList.containsKey(course)) {
+                coursesList.put(course, new ArrayList<>());
+                coursesList.get(course).add(student);
+            } else {
+                coursesList.get(course).add(student);
+            }
+            command = scanner.nextLine();
+        }
+        for (Map.Entry<String, List<String>> entry : coursesList.entrySet()) {
+            System.out.printf("%s: %d\n", entry.getKey(), entry.getValue().size());
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                System.out.printf("-- %s\n", entry.getValue().get(i));
+            }
+        }
+    }
+}
