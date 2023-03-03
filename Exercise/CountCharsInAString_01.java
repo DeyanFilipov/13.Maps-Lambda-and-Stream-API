7bf0bdbcce3caae1da6f9f181ff2993884707925
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-03-03 19:09:01 +0200

    Commit

---
+++ Exercise/CountCharsInAString_01.java
@@ -0,0 +1,22 @@
+package MapsLambdaAndStreamAPI.Exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class CountCharsInAString_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] charInput = scanner.nextLine().split(" ");
+
+        LinkedHashMap<Character, Integer> charsMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < charInput.length; i++) {
+            for (int j = 0; j < charInput[i].length(); j++) {
+                charsMap.putIfAbsent(charInput[i].charAt(j), 0);
+                charsMap.put(charInput[i].charAt(j), charsMap.get(charInput[i].charAt(j)) + 1);
+            }
+        }
+        charsMap.forEach((key, value) -> System.out.printf("%s -> %s\n", key, value));
+    }
+}
